eeaff867c5bf13f7e0f1f31ef457f6118d469cab
oleksandrasafranchuk <safrancukoleksandra@gmail.com>
2026-02-23 15:57:01 +0200

    Clean up secrets

--- Models/BookingSystemContext.cs
+++ Models/BookingSystemContext.cs
@@ -29,10 +29,6 @@ public partial class BookingSystemContext : DbContext
 
     public virtual DbSet<WorkspaceType> WorkspaceTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=booking_system;Username=postgres;Password=sasha20072103");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Booking>(entity =>
